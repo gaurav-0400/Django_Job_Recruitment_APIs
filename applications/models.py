1f9f9657e466ed3e8bfead10acae164d8b6c4939
gaurav <gauravrao0400@gmail.com>
2026-09-05 12:13:23 +0530

    create application and job model

--- applications/models.py
+++ applications/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.conf import settings
+from jobs.models import Job
+
+
+class Application(models.Model):
+    
+    class Status(models.TextChoices):
+        APPLIED = "APPLIED", "Applied"
+        SHORTLISTED = "SHORTLISTED", "Shortlisted"
+        REJECTED = "REJECTED", "Rejected"
+        HIRED = "HIRED", "Hired"
+
+    job = models.ForeignKey(
+        Job,on_delete=models.CASCADE,related_name="applications")
+
+    candidate = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="applications")
+
+    cover_letter = models.TextField(blank=True)
+
+    status = models.CharField(max_length=20,choices=Status.choices,default=Status.APPLIED)
+
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["job", "candidate"],
+                name="unique_job_candidate_application"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.candidate.username} - {self.job.title}"
--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+# from django.db import models
 
 # Create your models here.
+
+from django.db import models
+from django.conf import settings
+
+    
+class Job(models.Model):
+    class EmploymentType(models.TextChoices):
+        FULL_TIME= "FULL_TIME", "Full Time"
+        PART_TIME = "PART_TIME", "Part Time"
+        CONTRACT = "CONTRACT", "Contract"
+        INTERNSHIP = "INTERNSHIP", "Internship"
+
+
+    employer= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="jobs")
+
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    location = models.CharField(max_length=200)
+
+    skills = models.TextField(help_text="Comma separated skills")
+
+    employment_type = models.CharField(max_length=20,choices=EmploymentType.choices,default=EmploymentType.FULL_TIME)
+
+    salary = models.DecimalField(max_digits=12,decimal_places=2,null=True,blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
